15375070ce7a7fa0cebbb9496f538550538e9603
FaresGomaa1 <faresgomaa336@gmail.com>
2023-11-11 15:32:09 -0800

    Refactor _printf function and implement helper functions
    Split the _printf function into smaller, more modular parts to improve readability and adhere to the Betty style guidelines. Implemented helper functions print_char, print_string, and write_default to handle specific cases. Updated the header file with function prototypes. Resolves compilation errors due to implicit function declarations.

--- main.c
+++ main.c
@@ -1,50 +1,88 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
+#include <string.h>
 
 /**
- * _printf - Custom printf function
- * @format: Format string
- *
- * Return: Number of characters printed (excluding null byte)
- */
+* _printf - Custom printf function
+* @format: Format string
+*
+* Return: Number of characters printed (excluding null byte)
+*/
 int _printf(const char *format, ...)
 {
-	va_list args;
-	const char *ptr;
-	int count = 0;
+va_list args;
+const char *ptr;
+int count = 0;
 
-	va_start(args, format);
+va_start(args, format);
 
-	for (ptr = format; *ptr != '\0'; ptr++)
-	{
-		if (*ptr == '%' && *(ptr + 1) != '\0')
-		{
-			switch (*(ptr + 1))
-			{
-				case 'c':
-				count += write(1, va_arg(args, int), 1);
-				break;
-				case 's':
-				count += write(1, va_arg(args, char *), 1);
-				break;
-				case '%':
-				count += write(1, "%", 1);
-				break;
-				default:
-				count += write(1, "%", 1);
-				count += write(1, (ptr + 1), 1);
-				break;
-			}
-			ptr++;
-		}
-		else
-		{
-			count += write(1, ptr, 1);
-		}
-	}
+for (ptr = format; *ptr != '\0'; ptr++)
+{
+if (*ptr == '%' && *(ptr + 1) != '\0')
+{
+switch (*(ptr + 1))
+{
+case 'c':
+count += print_char(args);
+break;
+case 's':
+count += print_string(args);
+break;
+case '%':
+count += write(1, "%", 1);
+break;
+default:
+count += write_default(ptr);
+break;
+}
+ptr++;
+}
+else
+{
+count += write(1, ptr, 1);
+}
+}
 
-	va_end(args);
+va_end(args);
 
-	return (count);
+return (count);
+}
+
+/**
+* print_char - Helper function to print a character
+* @args: Argument list
+*
+* Return: Number of characters printed
+*/
+int print_char(va_list args)
+{
+char c = (char)va_arg(args, int);
+return (write(1, &c, 1));
+}
+
+/**
+* print_string - Helper function to print a string
+* @args: Argument list
+*
+* Return: Number of characters printed
+*/
+int print_string(va_list args)
+{
+char *str = va_arg(args, char *);
+return (write(1, str, strlen(str)));
+}
+
+/**
+* write_default - Helper function to write default case
+* @ptr: Current position in the format string
+*
+* Return: Number of characters printed
+*/
+int write_default(const char *ptr)
+{
+int count = 0;
+count += write(1, "%", 1);
+count += write(1, (ptr + 1), 1);
+return (count);
 }
--- main.h
+++ main.h
@@ -2,7 +2,10 @@
 #define MAIN_H
 
 #include <stdarg.h>
-
+/* task 0 */
 int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int write_default(const char *ptr);
 
 #endif /* MAIN_H */
