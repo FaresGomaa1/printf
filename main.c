09baa348eccc2e4da1b42012bbbcab72ed0599a9
FaresGomaa1 <faresgomaa336@gmail.com>
2023-11-11 12:39:39 -0800

    Implement _printf function
    This commit adds the _printf function as per the project requirements. The function supports the 'c', 's', and '%' conversion specifiers, providing basic printf functionality. Code follows the Betty style guidelines and has been tested with the provided main.c file.

---
+++ main.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _printf - Custom printf function
+ * @format: Format string
+ *
+ * Return: Number of characters printed (excluding null byte)
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	const char *ptr;
+	int count = 0;
+
+	va_start(args, format);
+
+	for (ptr = format; *ptr != '\0'; ptr++)
+	{
+		if (*ptr == '%' && *(ptr + 1) != '\0')
+		{
+			switch (*(ptr + 1))
+			{
+				case 'c':
+				count += write(1, va_arg(args, int), 1);
+				break;
+				case 's':
+				count += write(1, va_arg(args, char *), 1);
+				break;
+				case '%':
+				count += write(1, "%", 1);
+				break;
+				default:
+				count += write(1, "%", 1);
+				count += write(1, (ptr + 1), 1);
+				break;
+			}
+			ptr++;
+		}
+		else
+		{
+			count += write(1, ptr, 1);
+		}
+	}
+
+	va_end(args);
+
+	return (count);
+}
